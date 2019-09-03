2ff9aa3bfad39aa85a4563d2740ad0826e04cec1
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-09-03 15:38:57 +0100

    Add sign in page

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
   end
 
+  post '/names' do
+    @name_1 = params[:Player1]
+    @name_2 = params[:Player2]
+    erb :play
+  end
+  
   run! if app_file == $0
+
 end
+
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,16 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+# feature 'Testing infrastructure' do
+#   scenario 'Can run app and check page content' do
+#     visit('/')
+#     expect(page).to have_content 'Testing infrastructure working!'
+#   end
+# end
+
+feature 'Testing name form' do
+  scenario 'Can display submitted names' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in 'Player1', with: 'Bob'
+    fill_in 'Player2', with: 'Ben'
+    click_on 'Submit'
+    expect(page).to have_content 'Welcome Bob and Ben!'
   end
 end
