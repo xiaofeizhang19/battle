c414e0cb0909e28eb352325715365598821aeed0
Oliver Antcliff <antcliffoc@gmail.com>
2019-09-03 16:13:36 +0100

    add player 2 hit points

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -11,6 +11,16 @@ feature 'Testing name form' do
     fill_in 'Player1', with: 'Bob'
     fill_in 'Player2', with: 'Ben'
     click_on 'Submit'
-    expect(page).to have_content 'Welcome Bob and Ben!'
+    expect(page).to have_content 'Welcome Bob and Ben, FIGHT!'
+  end
+end
+
+feature "seeing player 2's hit points" do
+  scenario "at the start of game player 2 has 100 hit points" do
+    visit('/')
+    fill_in 'Player1', with: 'Bob'
+    fill_in 'Player2', with: 'Ben'
+    click_on 'Submit'
+    expect(page).to have_content "Ben's hit points: 100"
   end
 end
